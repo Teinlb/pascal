5b073893f926a6245b076bbb2aa7e81c6eee68c5
teinlb <tein.lebouille@gmail.com>
2024-09-14 12:39:31 +0200

    first

---
+++ main.pas
@@ -0,0 +1,18 @@
+program MyFirstProgram;
+
+// import statement
+uses crt;
+
+// main scope
+begin
+    {
+        This
+        is a
+        multi-line
+        comment
+    }
+
+    // single quotes & semicolon
+    writeln('Hello World');
+
+end.
